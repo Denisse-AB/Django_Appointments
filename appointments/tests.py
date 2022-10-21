36100ec5ab33143c91cc9d04c830f0c23ac6424d
Denisse Abreu <denisseabreu21@yahoo.com>
2022-10-20 21:18:02 -0400

    Test case django

--- appointments/tests.py
+++ appointments/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 from rest_framework import status
 from .models import App
 
@@ -28,12 +29,17 @@ class EmailTest(TestCase):
 
 class ResponseTest(APITestCase):
     url = reverse('create')
-    data = {'date': '2022-10-10', 'time': '1:00:00'}
+    data = { "email": "jane@doe.com", "name": "jane doe", "tel": 7874092828, "date": "2022-10-10", "time": "1:00:00"}
     
-    def test_the_create_appointment_400_response(self, url=url, data=data):
+    def test_the_create_appointment_201_response(self, url=url, data=data):
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_the_create_appointment_405_response(self, url=url, data=data):
-        response = self.client.get(url, data, format='json')
+    def test_the_create_appointment_405_response(self, url=url):
+        response = self.client.get(url, data={"name": "jane doe"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_the_create_appointment_400_response(self):
+        client = APIClient()
+        response = client.post('/post/', {'title': 'new idea'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
